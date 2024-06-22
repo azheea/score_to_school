9b716fe0f8d337f3693882f1a32fdaa3a44b1377
啊这 <87923452+azheea@users.noreply.github.com>
2024-06-22 09:59:00 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ import openpyxl
 import time
 from tqdm import tqdm
 import threading
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor  # 导入ThreadPoolExecutor
 import os
 import GetScore
-import Getschoolid
-
 # author 啊这.
 # buy me a coffie https://v我50.啊这.site
 
 # --------------------------------------
 province_id = 52  # 填写对应地区的id 贵州的为52 不清楚请填写省份全名(会浪费一点时间遍历)
-want = "计算机"  # 请填写所选专业
-year = 2023  # 此处填写所需获取的年份
+want = "生物"  # 请填写所选专业
+year = 2022  # 此处填写所需获取的年份
 is_hugescratch = True  # 是否模糊搜索 即包含专业关键词就收入
 is_college_only = True  # 仅保留大学
 # --------------------------------------
@@ -48,25 +46,32 @@ if year == None:
     year = input("请输入要获取的年份:")
 if not is_id_there:
     print("开始获取学校id")
-    all_school_id = Getschoolid(is_college_only)
+    all_school_id = {}
+    back = requests.get("https://static-data.gaokao.cn/www/2.0/info/linkage.json")
+    all_school_ids = ((json.loads(back.text)).get("data")).get("school")
+    for i in range(len(all_school_ids)):
+        school_id = all_school_ids[i]["school_id"]
+        school_name = all_school_ids[i]["name"]
+        if ("职业" in school_name or "技术" in school_name or "专科" in school_name) and is_college_only:
+            continue
+        all_school_id[school_name] = school_id
 
 if want == None:
     want = input("请输入您想选择的专业:")
-
 workbook = openpyxl.load_workbook(f'{path}\\schools.xlsx')
 worksheet = workbook['Sheet1']
 line = 0
-print("开始获取学校对应专业最低分,首次获取的速度较慢,请耐心等待")
+print("开始获取学校对应专业最低分,首次获取较慢,请耐心等待")
 start_time = time.time()
 total_lines = worksheet.max_row - 1
 lock = threading.Lock()
 errors = []
 def fetch_score(line):
-    school_name = worksheet.cell(row=line + 1, column=3).value
+    school_name = worksheet.cell(row=line + 1, column=4).value
     school_id = all_school_id.get(school_name, None)
     if school_id is not None:
         try:
-            worksheet.cell(row=line + 1, column=2).value = school_id
+            worksheet.cell(row=line + 1, column=3).value = school_id
             all_score = GetScore.getScore(schoolId=school_id,provinceId=province_id,year=year)
 
             result = {}
@@ -87,9 +92,9 @@ def fetch_score(line):
 
             if result.get("min") is not None and result.get("min_section") is not None:
                 with lock:
-                    worksheet.cell(row=line + 1, column=8).value = result.get("min")
-                    worksheet.cell(row=line + 1, column=9).value = result.get("min_section")
-                    worksheet.cell(row=line + 1, column=10).value = result.get("spname")
+                    worksheet.cell(row=line + 1, column=9).value = result.get("min")
+                    worksheet.cell(row=line + 1, column=10).value = result.get("min_section")
+                    worksheet.cell(row=line + 1, column=11).value = result.get("spname")
         except Exception as e:
             # print(e)
             errors.append(f"{school_name},{e}")
@@ -107,4 +112,5 @@ with tqdm(total=total_lines, ncols=80, dynamic_ncols=True) as pbar:
     pbar.close()
     print(f"\n工作完成,结果已经输出在{path}\{year}_{want}_学校分数排行.xlsx")
     print(f"本次用时: {end_time - start_time}秒")
-    print(f"{','.join(errors)}  获取失败")
+    if errors:
+        print(f"{','.join(errors)}  获取失败")
